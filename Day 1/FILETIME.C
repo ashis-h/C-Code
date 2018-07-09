002161a96087e2a762bab1abd8c51ecb6da9af34
ashis-h <32663261+ashis-h@users.noreply.github.com>
2018-07-09 12:23:29 +0530

    4

---
+++ Day 1/FILETIME.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Current File :%s\n",__FILE__);
+printf("Current Date :%s\n",__DATE__);
+printf("Current Time :%s\n",__TIME__);
+printf("Line Number :%d\n",__LINE__);
+getch();
+}
+
