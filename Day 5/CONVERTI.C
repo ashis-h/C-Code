fe2995c8e64f7bd0507ad2fa2d0bf0a24eab6647
ashis-h <32663261+ashis-h@users.noreply.github.com>
2018-07-15 21:40:45 +0530

    2

---
+++ Day 5/CONVERTI.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+int a[10],n,i;
+system("cls");
+clrscr();
+printf("enter the number to convert");
+scanf("%d",&n);
+for(i=0;n>0;i++)
+{
+a[i]=n%2;
+n=n/2;
+}
+printf("/n Binary of given Number is=");
+for(i=i-1;i>=0;i--)
+{
+printf("%d",a[i]);
+}
+getch();
+}
+
+
